977024ea758a15d5c45fc8e71287b52de4203d4f
Zederbear <hooverzed@gmail.com>
2024-09-05 12:56:42 -0600

    added comments to text_utils.py

--- text_utils.py
+++ text_utils.py
@@ -1,8 +1,11 @@
 def count_chars(text):
+    # returns the number of characters
     return len(text)
 
 def count_words(text):
+    # returns the amount of words by splitting by each space and counting the number of list items
     return len(text.split())
 
 def count_sentences(text):
+    # splits the text into a list by periods and returns how many list items there are
     return len(text.split("."))
