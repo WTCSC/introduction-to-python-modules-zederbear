a3d7db071bfbdfd3558d916f34c39404c23e3817
Zederbear <hooverzed@gmail.com>
2024-09-05 12:47:50 -0600

    added functionality to the functions in text_utils.py

--- text_utils.py
+++ text_utils.py
@@ -1,14 +1,8 @@
 def count_chars(text):
-    """
-    Count the number of characters in a text
-    """
+    return len(text)
 
 def count_words(text):
-    """
-    Count the number of words in a text
-    """
+    return len(text.split())
 
 def count_sentences(text):
-    """
-    Count the number of sentences in a text
-    """
+    return len(text.split("."))
